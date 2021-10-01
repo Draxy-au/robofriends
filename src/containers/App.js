8de54954d81858ca48994213b1252d86f945c681
William Hamilton <draxy80@gmail.com>
2021-10-01 18:55:42 +1000

    created robofriends web app

---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+
+import "../styles/App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.cypress.io/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    // we were checking if === 0. 0 = false, so check if not false
+    // then convert the if to a turnery
+    return !robots.length ? 
+      <h1>Loading...</h1>
+    : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+
+
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+import App from './containers/App';
 
 ReactDOM.render(
   <React.StrictMode>
